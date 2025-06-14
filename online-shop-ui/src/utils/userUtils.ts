cb0bfbde842069f67bb875c7f4957b97fd00a181
Ali Fathi <fathisut@gmail.com>
2025-06-15 03:23:21 +0800

    clear userUtils

--- online-shop-ui/src/utils/userUtils.ts
+++ online-shop-ui/src/utils/userUtils.ts
@@ -1,14 +1,1 @@
-import type {User} from "../models/User.ts";
-import {users} from "../data/users.ts";
 
-export function removeUser(target: User) {
-    users.filter(user => {return user !== target})
-}
-
-export function editUsername(user: User) {
-
-}
-
-export function editEmail(user: User) {
-
-}
