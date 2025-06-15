09cdd94dd8e462dc2fe8e9e63b65d40c85a7177f
mehdi95zeid <mehdi95zeid@gmail.com>
2025-06-15 13:32:48 +0330

    modified:   src/components/Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import type { PropsWithChildren } from "react";
 // import User from "./Layout/User";
 import Menu from "./Layout/Menu";
-import Admin from "./Layout/Admin";
+// import Admin from "./Layout/Admin";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
@@ -9,7 +9,7 @@ const Layout = ({ children }: PropsWithChildren) => {
       <aside className="fixed z-10 right-0 min-h-screen p-2 bg-base-300">
         <Menu />
         {/* <User /> */}
-        <Admin />
+        {/* <Admin /> */}
       </aside>
       <main>{children}</main>
     </div>
