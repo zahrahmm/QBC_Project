56f9d72fff391a2fb1a2017d296cb0e90ca01fe0
Zahra Homami <zahra28582@gmail.com>
2025-06-28 16:45:41 +0330

    part of the view and register comments was done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-// import { useState } from "react";
+import { useState } from "react";
 import Layout from "./components/Layout";
+import TabSelector from "./components/TabSelector";
+import ReviewForm from "./components/ReviewForm";
+import ReviewList from "./components/ReviewList";
 
 function App() {
+  const [activeTab, setActiveTab] = useState<"view" | "add" | "related">(
+    "view"
+  );
   return (
     <Layout>
       <div>
         <h1>Quera</h1>
+        <div className="flex gap-10 p-8">
+          <TabSelector activeTab={activeTab} setActiveTab={setActiveTab} />
+          <div className="flex-1">
+            {activeTab === "add" && <ReviewForm />}
+            {activeTab === "view" && <ReviewList />}
+            {activeTab === "related" && (
+              <p>در اینجا محصولات مرتبط نمایش داده می‌شوند.</p>
+            )}
+          </div>
+        </div>
       </div>
     </Layout>
   );
