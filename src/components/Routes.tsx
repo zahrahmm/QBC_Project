3ca68c81b47b675a6241441f5c0464e8c7df3751
mehdi95zeid <mehdi95zeid@gmail.com>
2025-06-28 21:42:43 +0330

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Routes.tsx 	new file:   src/index.css 	modified:   src/main.tsx 	deleted:    src/pages/Cart.tsx

--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from "react-router";
-import Cart from "../pages/Cart";
-import Home from "../pages/Home";
-import Shop from "../pages/Shop";
-import NotFound from "../pages/NotFound";
+import CartPage from "../Pages/CartPage";
+import Home from "../Pages/Home";
+import Shop from "../Pages/Shop";
+import NotFound from "../Pages/NotFound";
 
 function RoutesComponent() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/Shop" element={<Shop />} />
-      <Route path="/Cart" element={<Cart />} />
-      <Route path="/Fave" element={<Fave />} />
+      <Route path="/Cart" element={<CartPage />} />
+      {/* <Route path="/Fave" element={<Fave />} />
       <Route path="/Login" element={<Login />} />
-      <Route path="/Register" element={<Register />} />
+      <Route path="/Register" element={<Register />} /> */}
 
       <Route path="*" element={<NotFound />} />
     </Routes>
