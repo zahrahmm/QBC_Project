0434ff8465e2730fc3fd3507f58ecd30fdbc6b12
Saba Fakhri <sabafakhri71@gmail.com>
2025-07-05 22:16:34 +0300

    NEW Carousel

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,28 @@
+const ProductCard = () => {
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title relative">
+          Card Title
+          <div className="badge badge-secondary absolute left-6">NEW</div>
+        </h2>
+        <p>
+          A card component has a figure, a body part, and inside body there are
+          title and actions parts
+        </p>
+        <div className="card-actions justify-end">
+          <div className="badge badge-outline">Fashion</div>
+          <div className="badge badge-outline">Products</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/components/Routes.tsx
+++ src/components/Routes.tsx
@@ -1,17 +1,18 @@
 import { createBrowserRouter } from "react-router";
-import CartPage from "../Pages/CartPage";
-import Home from "../Pages/Home";
-import Shop from "../Pages/Shop";
-import NotFound from "../Pages/NotFound";
+import CartPage from "../pages/CartPage";
+import Home from "../pages/Home";
+import Shop from "../pages/Shop";
+import NotFound from "../pages/NotFound";
 import Layout from "./Layout/Layout";
-import RegisterPage from "../Pages/RegisterPage";
-import LoginPage from "../Pages/LoginPage";
-import CreateNewProduct from "../Pages/CreateNewProduct";
-import UpdateProfile from "../Pages/UpdateProfile";
-import Dashboard from "../Pages/Dashboard";
-import orders from "../Pages/orders";
+import RegisterPage from "../pages/RegisterPage";
+import LoginPage from "../pages/LoginPage";
+import CreateNewProduct from "../pages/CreateNewProduct";
+import UpdateProfile from "../pages/UpdateProfile";
+import Dashboard from "../pages/Dashboard";
+import orders from "../pages/orders";
 import AdminRoute from "./AdminRoute";
 import UserRoute from "./UserRoute";
+import Carousel from "./Carousel";
 
 const Routes = createBrowserRouter([
   {
@@ -25,6 +26,7 @@ const Routes = createBrowserRouter([
       { path: "Login", Component: LoginPage },
       { path: "cart", Component: CartPage }, // سفرشات وقتی کاربر (وارد نشده) گرفته میشود و پس از وارد شدن کاربر به سفارشات کاربر اضافه میشود
       { path: "Shop", Component: Shop },
+      { path: "Carousel", Component: Carousel },
       {
         Component: AdminRoute,
         children: [
