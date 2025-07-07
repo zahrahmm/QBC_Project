056b1ca97c9c0c51cd1b0011ae4f4542f10ffb07
Zahra Homami <zahra28582@gmail.com>
2025-07-07 20:11:43 +0330

    view and register comments completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 // import { useState } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import RoutesComponent from "./components/Routes";
-import ProductPage from "./components/productPage/ProductPage";
 
 function App() {
   return (
     <Router>
       <RoutesComponent />
-      <ProductPage />
     </Router>
   );
 }
