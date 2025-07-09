5b7fa5929f6d3b16dad2be6289acd78f431acaf1
Saba Fakhri <sabafakhri71@gmail.com>
2025-07-09 18:45:38 +0300

    fix productCard component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,10 @@ const Home = () => {
   if (user?.isAdmin) return <Navigate to="/dashboard" />;
 
   return (
-    <div className="grid grid-cols-2 px-20 pt-20 gap-16">
+    <div className="grid grid-cols-2  px-20 pt-20 gap-16">
       <div className="grid grid-cols-2 gap-6 ">
         <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        
       </div>
       <div className="col-span-1">
         <Carousel />
