561aabb83e4ee3041cf5be132e87ef0058b9ceec
ahz <ah.ziedabadi@gmail.com>
2025-07-10 19:25:42 +0330

    modified:   src/Pages/AllProduct.tsx

--- src/Pages/AllProduct.tsx
+++ src/Pages/AllProduct.tsx
@@ -1,15 +1,14 @@
-
-
 import { useNavigate } from "react-router-dom";
 import useProducts from "../utils/use-products";
 import useProductStore from "../stores/useProductStore";
-
 
 const AllProduct = () => {
   const navigate = useNavigate();
   const { data: products, isLoading, isError } = useProducts();
 
-  const setSelectedProductId = useProductStore((state) => state.setSelectedProductId);
+  const setSelectedProductId = useProductStore(
+    (state) => state.setSelectedProductId
+  );
 
   const persianDateFormatter = new Intl.DateTimeFormat("fa-IR", {
     year: "numeric",
@@ -17,12 +16,20 @@ const AllProduct = () => {
     day: "numeric",
   });
 
-
-  if (isLoading) return <div className="   text-center mt-10 ">در حال بارگذاری<span className="loading loading-dots loading-md mr-1"></span></div>;
-  if (isError) return <div className=" text-center mt-10 ">خطا در بارگذاری اطلاعات محصول!</div>;
+  if (isLoading)
+    return (
+      <div className="   text-center mt-10 ">
+        در حال بارگذاری
+        <span className="loading loading-dots loading-md mr-1"></span>
+      </div>
+    );
+  if (isError)
+    return (
+      <div className=" text-center mt-10 ">خطا در بارگذاری اطلاعات محصول!</div>
+    );
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center px-8 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center p-22">
       {products?.map((product) => (
         <div
           key={product._id}
@@ -40,7 +47,9 @@ const AllProduct = () => {
             <span className="text-sm self-end ">
               {persianDateFormatter.format(
                 new Date(
-                  product.updatedAt !== product.createdAt ? product.updatedAt : product.createdAt
+                  product.updatedAt !== product.createdAt
+                    ? product.updatedAt
+                    : product.createdAt
                 )
               )}
             </span>
@@ -69,4 +78,3 @@ const AllProduct = () => {
 };
 
 export default AllProduct;
-
